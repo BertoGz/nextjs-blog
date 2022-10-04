ba5db2408da75017db2ebde2c22c2b354158b5f5
bertogz <bertogzlx@gmail.com>
2022-10-04 08:34:45 -0700

    add id dynamic page

---
+++ pages/posts/[id].js
@@ -0,0 +1,29 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostById } from "../../lib/posts";
+
+export async function getStaticProps({ params }) {
+  const postData = getPostById(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
+}
